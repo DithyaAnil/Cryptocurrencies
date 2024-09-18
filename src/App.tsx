7f9526dc8980099daccecc63d02cb799117e2b7b
DithyaAnil <adithyaanil002@gmail.com>
2024-09-19 00:09:30 +0530

    TypeScript and Axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export type Crypto = {
+  id: string;
+  name: string;
+  price_usd: string;
+  symbol: string;
+  volume24: number;
+  volume24a: number;
+};
 
 function App() {
+  const [cryptos, setCryptos] = useState<Crypto[] | null>();
+  useEffect(() => {
+    const url = "https://api.coinlore.net/api/tickers/";
+    axios(url).then((response) => {
+      setCryptos(response.data.data);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {cryptos
+        ? cryptos.map((crypto) => {
+            return <p>{crypto.name + " :" + "$" + crypto.price_usd}</p>;
+          })
+        : null}
     </div>
   );
 }
