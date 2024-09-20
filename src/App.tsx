b947145aa7ce0d2b30d562b47668f8326143edb2
DithyaAnil <adithyaanil002@gmail.com>
2024-09-20 23:23:19 +0530

    Pie Chart with Chart.js

--- src/App.tsx
+++ src/App.tsx
@@ -4,47 +4,18 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import CryptoSummary from "./components/CryptoSummary";
 import { Crypto } from "./Types";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
 import type { ChartData, ChartOptions } from "chart.js";
 import moment from "moment";
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 function App() {
   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
   const [selected, setSelected] = useState<Crypto[]>([]);
-  /* const [data, setData] = useState<ChartData<"line">>();*/
+  const [data, setData] = useState<ChartData<"pie">>();
   const [range, setRange] = useState<number>(30);
-  /*const [options, setOptions] = useState<ChartOptions<"line">>({
-    responsive: true,
-    plugins: {
-      legend: {
-        display: false,
-      },
-      title: {
-        display: true,
-        text: "Chart.js Line Chart",
-      },
-    },
-  });
-  */
+
   useEffect(() => {
     const url =
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
@@ -103,6 +74,33 @@ function App() {
 
   useEffect(() => {
     console.log("SELECTED", selected);
+    if (selected.length === 0) return;
+    setData({
+      labels: selected.map((s) => s.name),
+      datasets: [
+        {
+          label: "# of Votes",
+          data: selected.map((s) => s.owned * s.current_price),
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(254, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132,1)",
+            "rgba(54, 162, 235,1)",
+            "rgba(255, 206, 86,1)",
+            "rgba(75, 192, 192,1)",
+            "rgba(153, 102, 2552,1)",
+            "rgba(255, 159, 64,1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    });
   }, [selected]);
 
   function updateOwned(crypto: Crypto, amount: number): void {
@@ -143,11 +141,11 @@ function App() {
       })}
 
       {/*selected ? <CryptoSummary crypto={selected} /> : null*/}
-      {/*data ? (
+      {data ? (
         <div style={{ width: 600 }}>
-          <Line options={options} data={data} />
+          <Pie data={data} />
         </div>
-      ) : null*/}
+      ) : null}
 
       {selected
         ? "Your portfolio is worth: $" +
